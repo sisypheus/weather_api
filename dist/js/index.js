fd28d1d36540e9bfb018f38eb6685bf8205f732a
Theo Poette <55665426+sisypheus@users.noreply.github.com>
2022-02-07 10:43:54 +0100

    Update index.js

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,5 @@
 import {API_KEY} from './secrets.js';
 
-//const API_KEY = '9c08c70c211d111c1764de9691e3c902';
-
 const request = async url => {
     const response = await fetch(url);
     return response.ok ? response.json() : Promise.reject({error: 500});
